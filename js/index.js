398bc316c08a2c1ff454cf2aa37add35a96c4ac0
김수임 | Suim Kim <suim0215@gmail.com>
2022-10-11 01:57:34 +0900

    이미지 효과 변경

--- js/index.js
+++ js/index.js
@@ -8,11 +8,38 @@ $(document).ready(function() {
             $('#' + activeTab).addClass('on');
         })
     });
-    
-    $('.slider').bxSlider({
-        mode:'horizontal',
-          speed:1000, 
-          auto: true
+        
+
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("slow");
+    });
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+    $("#flip3").click(function(){
+        $("#panel3").slideToggle("slow");
+    });
+    $("#flip4").click(function(){
+        $("#panel4").slideToggle("slow");
+    });
+    $("#flip5").click(function(){
+        $("#panel5").slideToggle("slow");
+    });
+    $("#flip6").click(function(){
+        $("#panel6").slideToggle("slow");
+    });
+    $("#flip7").click(function(){
+        $("#panel7").slideToggle("slow");
+    });
+    $("#flip8").click(function(){
+        $("#panel8").slideToggle("slow");
+    });
+
+    $(".image img").hover(function(){
+        $(this).css("opacity", 1);
+    }, function(){$(this).css("opacity", 0.4);
     });
     
-});
+    
+
+}); 
